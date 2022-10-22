b04761e5c96a5fe40318f9c2d9c7c1676729c06a
Aditya Pawar <75765885+adityapawar50@users.noreply.github.com>
2022-10-22 05:25:48 -0500

    home landing start

--- spin_spec2/src/pages/Home.js
+++ spin_spec2/src/pages/Home.js
@@ -1,7 +1,50 @@
 import Navbar from "../components/Navbar";
-const Home = () => {
-    return <div>home please work<Navbar></Navbar></div>
-  };
+// const Home = () => {
+//     return <div>home please work<Navbar></Navbar></div>
+//   };
   
-  export default Home;
+//   export default Home;
+  
+
+  export default function Home() {
+    // let navigate = useNavigate();
+  
+    // function handleJuror() {
+    //   navigate("/JLogin");
+    // }
+  
+    // function handleAdmin() {
+    //   navigate("/AdminLogin");
+    // }
+  
+    return (
+      <div className="bg-offWhite">
+        {/* <Navbar /> */}
+        <div class="flex h-screen">
+          <div className="flex flex-col m-auto">
+            <div className="shadow-lg rounded-lg w-[500px] p-3">
+              <div className="space-y-4">
+                <div className="text-6xl font-bold">Hello!</div>
+                <div className="text-lg"> Please Select Account</div>
+                <div className="flex flex-col space-y-4 py-4">
+                  <button
+                    onClick={console.log("hi")}
+                    className="btn text-3xl py-4 px-3"
+                  >
+                    Juror
+                  </button>
+                  <button
+                    onClick={console.log("hi")}
+                    className="btn text-3xl py-4 px-3"
+                  >
+                    Admin
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   
