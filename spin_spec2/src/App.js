619790ae692d1d4f4a939888a76fd59ef1a4d836
Aditya Pawar <75765885+adityapawar50@users.noreply.github.com>
2022-10-22 06:02:22 -0500

    page routing update starting page good

--- spin_spec2/src/App.js
+++ spin_spec2/src/App.js
@@ -4,7 +4,7 @@ import './app12.css';
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
@@ -16,20 +16,13 @@ import "./app12.css";
 function App() {
 
   return (
-    <div className = "common-background">
-      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
-          </Route><Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
           <Route path="admin" element={<Admin />} />
           <Route path="user" element={<User />} />
           <Route path="AdminInput" element={<AdminInput />} />
           <Route path="*" element={<NoPage />} />
-        </Route>
+          <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
-    </div>
   );
 }
 
--- spin_spec2/src/pages/Home.js
+++ spin_spec2/src/pages/Home.js
@@ -1,5 +1,8 @@
 import Navbar from "../components/Navbar";
 import "../app12.css";
+import { Outlet, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
 // const Home = () => {
 //     return <div>home please work<Navbar></Navbar></div>
 //   };
@@ -8,26 +11,23 @@ import "../app12.css";
   
 
   export default function Home() {
+    // function handleAdmin() {
+    //   navigate("/Admin");
+    // }
     return (
       <div className = "admin">
           <h1 className="dashboard-title">
               Welcome!
               Are You a user or Admin
           </h1>
-          <div className = "overall">
-              {/* {users.map((user,i) => {
-                  return (
-                      <CurrentUser 
-                          key={i}
-                          id={user._id}
-                          name={user.name}
-                      />
-                  );
-              })} */}
-          </div>
-          <h1 className="dashboard-title">
-              Events Viewer
-          </h1>
+          {/* <h1 className="dashboard-title">
+            <button
+                  onClick={handleAdmin}
+                  className="btn text-3xl py-4 px-3"
+                >
+                  Juror
+            button </button>
+          </h1> */}
       </div>
   );
   
